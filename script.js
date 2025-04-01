9b491bfc32802d62faa80040f066d40cba81caa4
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-04-01 10:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+async function DelayTime(event){
+	event.preventDefault();
+	
+	let textinput = document.getElementById("text").value;
+	let outputdelay = document.getElementById("delay").value;
+
+	let delaydisplay = parseInt(outputdelay);
+
+	if(delaydisplay < 0){
+		alert("Please enter valid delay")
+		return;
+	}
+
+	await new Promise(resolve => setTimeout(resolve, delaydisplay));
+
+	const outputDiv = document.getElementById('output');
+	outputDiv.innerText = textinput;
+	
+}
+
+document.getElementById('btn').addEventListener('click', DelayTime);
